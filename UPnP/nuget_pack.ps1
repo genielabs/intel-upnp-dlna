5c1280556d0198ae968289c98423171be8d9c987
Gene <genemars@users.noreply.github.com>
2022-12-19 19:29:46 +0100

    Upgraded to .NET 6

--- UPnP/nuget_pack.ps1
+++ UPnP/nuget_pack.ps1
@@ -1,18 +1,17 @@
 $project = "UPnP"
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-#$version = [System.Reflection.Assembly]::LoadFile("$root\$project\bin\Debug\$project.dll").GetName().Version
 
 $versionStr = "{0}" -f ($env:APPVEYOR_REPO_TAG_NAME)
 
 if (-not ([string]::IsNullOrEmpty($versionStr))) {
-  Write-Host "Setting $project .nuspec version tag to $versionStr"
+  Write-Host "Setting $project .csproj version tag to $versionStr"
 
-  $content = (Get-Content $root\$project\$project.nuspec) 
-  $content = $content -replace '\$version\$',$versionStr
+  $content = (Get-Content $root\$project\$project.csproj) 
+  $content = $content -replace '(?<=\<Version\>).*?(?=\</Version\>)',$versionStr
 
-  $content | Out-File $root\$project\$project.compiled.nuspec
+  $content | Out-File $root\$project\$project.csproj
 
-  & nuget pack $root\$project\$project.compiled.nuspec
+  & dotnet pack $root\$project -o .
 }
 else {
   Write-Host "Version string is empty, possibly dry run or APPVEYOR_REPO_TAG_NAME environment variable is not set"
